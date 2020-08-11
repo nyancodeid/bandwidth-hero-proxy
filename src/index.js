c61e51def5d1571b26a1282c6f054b94c4a21682
Ryan Aunur <ryandevstudio@gmail.com>
2020-08-11 16:27:37 +0700

    update(index): add logInfo and env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import { signale } from "@src/config/signale.js";
+import { signale, logInfo } from "@src/config/signale.js";
+import Env from "@src/config/env.js";
 
+logInfo("APP", `App Key: ${Env.use("APP_KEY")}`);
 signale.info(`[APP] It's work!`);
