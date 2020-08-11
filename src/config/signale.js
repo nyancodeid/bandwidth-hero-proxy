ff8b58af00cdc5210f6452b7d2054ca987363aa2
Ryan Aunur <ryandevstudio@gmail.com>
2020-08-11 16:27:48 +0700

    update(signale): add method logInfo

--- src/config/signale.js
+++ src/config/signale.js
@@ -3,3 +3,11 @@ import { Signale } from "signale";
 const options = {};
 
 export const signale = new Signale(options);
+
+/**
+ * Log info with service name
+ * @param {string} service
+ * @param {string} message
+ */
+export const logInfo = (service, message) =>
+  signale.info(`[${service}] ${message}`);
