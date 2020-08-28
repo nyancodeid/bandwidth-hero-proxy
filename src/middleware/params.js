89362973946c8c2877dbb08deacf2898f602d91c
Ryan Aunur <ryandevstudio@gmail.com>
2020-08-28 21:30:54 +0700

    update: add more jsdoc

--- src/middleware/params.js
+++ src/middleware/params.js
@@ -1,7 +1,21 @@
+/**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef {import('express').NextFunction} NextFunction
+ */
+
 import Env from "@src/config/env.js";
 
 export const params = (req, res, next) => {
   let url = req.query.url;
+/**
+ * @middleware
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+export const params = async (req, res, next) => {
+  let url = req.query?.url;
   if (Array.isArray(url)) {
     url = url.join("&url=");
   }
