f3b4b110ebefcf81884646d122273c48a92985f5
Ryan Aunur <ryandevstudio@gmail.com>
2020-09-02 19:25:07 +0700

    update: env string to number

--- src/middleware/params.js
+++ src/middleware/params.js
@@ -38,7 +38,7 @@ export const params = async (req, res, next) => {
   req.params.webp = !req.query?.jpeg;
   req.params.grayscale = req.query?.bw != 0;
   req.params.quality =
-    parseInt(req.query?.l, 10) || Env.use("APP_DEFAULT_QUALITY", 40);
+    Number(req.query?.l) || Number(Env.use("APP_DEFAULT_QUALITY", 40));
 
   next();
 };
