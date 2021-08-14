5fcb9cb2bdab863a62082f399190312c6b14f0bd
Ryan Aunur <ryandevstudio@gmail.com>
2021-08-14 15:21:53 +0700

    update(config/sqlite): use env database name

--- src/config/sqlite.js
+++ src/config/sqlite.js
@@ -1,9 +1,12 @@
 import Knex from "knex";
+import Env from "@src/config/env.js";
+
+const DATABASE_NAME = Env.use("DB_FILENAME");
 
 export const db = Knex({
   client: "sqlite3",
   connection: {
-    filename: "./__db__/database.dev.sqlite3",
+    filename: "./__db__/" + DATABASE_NAME,
   },
   useNullAsDefault: true,
 });
